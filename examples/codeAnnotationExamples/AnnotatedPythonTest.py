fb8a5204d4f341ceb11144d763851ef13ac6c623
Dirk Lehmann <doergn@users.noreply.github.com>
2019-09-26 13:48:55 +0200

    Fix missing intent (which made the example invalid)

--- examples/codeAnnotationExamples/AnnotatedPythonTest.py
+++ examples/codeAnnotationExamples/AnnotatedPythonTest.py
@@ -16,4 +16,4 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
-        s.split(2)
+            s.split(2)
